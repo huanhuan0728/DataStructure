4f73e2c83de617469d662e1f7b8f8cf9485f8380
xuhuan lu <1477405138@qq.com>
2024-09-27 22:59:27 +0800

    Initial Commit

---
+++ tree/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tree
+//
+//  Created by xuhuan lu on 2024/9/27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
